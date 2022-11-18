65d49c3a5f5e72a128547aa03e0e4151554ae3c7
hmhtom <hmhtom@gmail.com>
2022-11-17 21:33:45 -0500

    about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,17 @@
 import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 function About() {
-  return <Col>This is the About me Page</Col>;
+  return (
+    <>
+      <Col className="col-md-12 col-lg-6">
+        <Image src="./Assets/images/avatar.jpg" roundedCircle thumbnail />
+      </Col>
+      <Col className="col-md-12 col-lg-6 fs-4">
+        Hi Im Alex, I am a full stack developer currently studying in U of T.
+      </Col>
+    </>
+  );
 }
 
 export default About;
